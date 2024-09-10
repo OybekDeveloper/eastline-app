d2c82912c656e7604ba976c356183d198549136c
OybekDeveloper <programmeroybek211@gmail.com>
2024-09-11 01:24:20 +0500

    ddd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
   // reactStrictMode: false,
   webpack(config) {
     config.resolve.fallback = {
