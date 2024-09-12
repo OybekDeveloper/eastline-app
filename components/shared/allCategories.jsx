a8f16ad9bffcff48c4aebcd46d634829d8f9a479
OybekDeveloper <programmeroybek211@gmail.com>
2024-09-12 17:49:05 +0500

    fixeding

--- components/shared/allCategories.jsx
+++ components/shared/allCategories.jsx
@@ -15,14 +15,19 @@ const AllCategories = ({ categories }) => {
                 key={i}
                 className="basis-[35%] md:basis-[25%] lg:basis-[20%] mr-5"
               >
-                <Link href={`/${item.topCategoryId}/${item.id}`} className="px-3 flex flex-col items-center gap-y-1">
-                  <CustomImage
-                    src={`${item.image}`}
-                    width={100}
-                    height={100}
-                    alt={`banner-img-${item.id}`}
-                    className="w-full mx-auto aspect-square object-contain"
-                  />
+                <Link
+                  href={`/${item.topCategoryId}/${item.id}`}
+                  className="px-3 flex flex-col justify-center items-center gap-y-1"
+                >
+                  <div className="relative h-[100px] md:h-[200px] overflow-hidden flex justify-center items-center">
+                    <CustomImage
+                      src={`${item.image}`}
+                      width={100}
+                      height={100}
+                      alt={`banner-img-${item.id}`}
+                      className="w-full mx-auto  object-contain"
+                    />
+                  </div>
                   <p className="textSmall3 font-semibold">{item.name}</p>
                 </Link>
               </CarouselItem>
