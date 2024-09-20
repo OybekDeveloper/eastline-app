dbe4c4164c19c2b7d2780c5e13d52124accffa6d
OybekDeveloper <programmeroybek211@gmail.com>
2024-09-20 18:20:40 +0500

    delete price

--- app/not-found.js
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function NotFound() {
-  return (
-    <div className="w-full flex justify-center items-center my-10">
-      <h1 className="font-bold textBig">Сайт находится в разработке</h1>
-    </div>
-  );
-}
