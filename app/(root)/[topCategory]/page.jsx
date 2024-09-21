45af1b1f98f17bde4bce204d88df3e613a21067c
OybekDeveloper <programmeroybek211@gmail.com>
2024-09-21 16:43:30 +0500

    delete price

--- app/(root)/[topCategory]/page.jsx
+++ app/(root)/[topCategory]/page.jsx
@@ -1,7 +1,7 @@
 import NotFoundComponent from "@/components/not-found/not-found";
 
 export default function Page() {
-  return <div>
+  return <div className="flex justify-center items-center">
       <NotFoundComponent/>
   </div>;
 }
