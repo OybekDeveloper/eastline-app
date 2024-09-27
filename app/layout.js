884df5ecd4a462df53ed0ceb5513787d55f186f5
OybekDeveloper <programmeroybek211@gmail.com>
2024-09-27 17:23:22 +0500

    suka

--- app/layout.js
+++ app/layout.js
@@ -41,7 +41,8 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   const topCategories = await ApiService.getData("/api/topCategory", "topCategory");
   const productsData = await ApiService.getData("/api/product", "product");
-
+  console.log("Product data:" + productsData);
+  
   return (
     <html lang="en">
       <body
