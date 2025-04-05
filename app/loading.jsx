e1f946577e9f1a6801be160c71fa29ab08816aa3
OybekDeveloper <Programmeroybek211@gmail.com>
2025-04-04 23:24:17 -0700

    fix it pls work

--- app/loading.jsx
+++ app/loading.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion";
 export default function Loading() {
   return (
     <div className="fixed top-0 left-0 z-[999999] bg-white w-screen h-screen justify-center items-center flex">
-      <div className="relative flex w-64 animate-pulse gap-2 p-4">
+      <div className="relative flex w-64 animate-pulse gap-2 p-4 dasfadf">
         <Image
           className="px-4 w-72"
           src={Logo}
